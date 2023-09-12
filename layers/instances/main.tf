929ec8ebed4cdab4fb6da2d7d823470b1720b67b
Anis FATHALLAH <anis@fathallah.fr>
2023-09-12 14:52:31 +0400

    use source_snapshot for certain disks

--- layers/instances/main.tf
+++ layers/instances/main.tf
@@ -1,6 +1,8 @@
 module "instance_template" {
-  source               = "terraform-google-modules/vm/google//modules/instance_template"
-  version              = "8.0.1"
+  # source               = "terraform-google-modules/vm/google//modules/instance_template"
+  # version              = "8.0.1"
+  source = "git::https://github.com/anisf/terraform-google-vm.git?ref=feature%2Fit_disk_from_snapshot"
+
   project_id           = var.instances_project_id
   for_each             = var.instance_roles
   subnetwork           = data.terraform_remote_state.network.outputs.subnets["${var.region}/${each.value.subnetwork}"].name
